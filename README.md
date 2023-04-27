b2b95f4c2f8bd4409c61a94a19d6856092b16ce5
Rishab Garg <rishgarg@gmail.com>
2023-04-27 10:22:14 +0200

    feat!: added perl implementation and ported

--- README.md
+++ README.md
@@ -1,14 +1,48 @@
 # crpreview
 
-TODO: Write a description here
+Preview script made for the [lf file manager](https://github.com/gokcehan/lf). May work with other terminal file manageres after some configuration.
+
+## Previews
+
+| File type    | Tool       |
+| ------------ | ---------- |
+| archives[^1] | `tar`      |
+| markdown     | `glow`     |
+| images       | `chafa`    |
+| pdf          | `pdftoppm` |
+| text         | `bat`      |
+
+[^1]: Supported formats: `tar`, `7-zip`, `zip`, `bzip`,`bzip2`, `gunzip`, `xz`, `zstd`, `lzip`, `lrzip`
+
+## Requirements
+
+In addition to the preview tools stated above, these are the platform specific requirements:
+
+### MacOS
+
+Nothing. It is however recommended, to install the tools above with `brew`.
+
+### GNU/\*
+
+- libarchive (bsdtar)
+
+### Windows
+
+-> Use WSL2.
 
 ## Installation
 
-TODO: Write installation instructions here
+1. Build the executable using `make``
+2. Copy the path to this working directory on your system`
 
 ## Usage
 
-TODO: Write usage instructions here
+Add the following lines to your `lfrc`:
+
+```conf
+set previewer /path/to/crpreview/bin/crpreview
+map i $ /path/to/crpreview/bin/crpreview $f | less -R
+```
 
 ## Development
 
--- src/crpreview.cr
+++ src/crpreview.cr
@@ -1,6 +1,64 @@
 # TODO: Write documentation for `Crpreview`
 module Crpreview
-  VERSION = "0.1.0"
+  # darwin/bsd or GNU system commands
+  stat = ""
+  tar = ""
+  {% if flag?(:darwin) %}
+    stat = "stat -f '%N%i%T%z%B%m'"
+    tar = "tar"
+  {% elsif flag?(:bsd) %}
+    stat = "stat -f '%N%i%T%z%B%m'"
+    tar = "tar"
+  {% elsif flag?(:gnu) %}
+    stat = "stat  --printf '%n%i%F%s%W%Y'"
+    tar = "bsdtar"
+  {% else %}
+    print "ERROR: Unknown system. Please make sure your system is supported by this previewer."
+    exit
+  {% end %}
 
-  # TODO: Put your code here
+  file = ARGV[0]
+
+  class Image
+    def initialize(width : String, height : String)
+      @res = "#{width.to_i - 5}x#{height}"
+    end
+
+    def symbols(file : String)
+      print `chafa "#{file}" -s #{@res} -f symbols`
+    end
+  end
+
+  case file
+  when /.tar.lrz$/
+    print `#{tar} -tvf "#{file}"`
+    exit
+  when /.md$/
+    # print `glow "#{file}"`
+    print `bat -pp --color always --wrap character --  "#{file}"`
+    exit
+  end
+
+  type = `file -b --mime-type "#{file}"`
+
+  case type
+  when /^image/
+    image = Image.new(ARGV[1], ARGV[2])
+    image.symbols(file)
+  when /inode\/directory/
+    print `exa -a --color=always --group-directories-first --icons "#{file}"`
+  when /x-tar$|x-7z-compressed$|zip$|x-bzip$|x-bzip2$|gzip$|x-xz$|zstd$|x-lzip$/
+    print `#{tar} -tvf "#{file}"`
+  when /application\/pdf/
+    hash = `#{stat} -- "#{file}" | sha256sum`.split(' ')[0]
+    cache = "#{ENV["HOME"]}/.cache/lf/thumbnail.#{hash}"
+
+    `[ ! -f "#{cache}.jpg" ] && pdftoppm -jpeg -f 1 -singlefile "#{file}" "#{cache}"`
+    image = Image.new(ARGV[1], ARGV[2])
+    image.symbols("#{cache}.jpg")
+  when /application\/octet-stream/
+    print "binary data"
+  else
+    print `bat -pp --color always --wrap character -- "#{file}"`
+  end
 end
